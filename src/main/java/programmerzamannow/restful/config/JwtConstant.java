f41e23ed850b7b5bf1c65be781d14f02a0984030
iqra94 <regeraiqra@gmail.com>
2023-10-03 22:18:52 +0700

    buat appConfig, jwtValidator

---
+++ src/main/java/programmerzamannow/restful/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package programmerzamannow.restful.config;
+
+public class JwtConstant {
+
+  public static final String SECRET_KEY = "asdalkasjfemrtnadsfemrntq";
+  public static final String JWT_HEADER = "Authorization";
+
+}
