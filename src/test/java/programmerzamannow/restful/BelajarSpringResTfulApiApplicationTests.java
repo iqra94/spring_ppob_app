98bc129a33b697525d1289edc4da8b8f4e97f714
iqra94 <regeraiqra@gmail.com>
2023-10-03 21:01:21 +0700

    beres setup awal

--- src/test/java/programmerzamannow/restful/BelajarSpringResTfulApiApplicationTests.java
+++ src/test/java/programmerzamannow/restful/BelajarSpringResTfulApiApplicationTests.java
@@ -3,11 +3,11 @@ package programmerzamannow.restful;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BelajarSpringResTfulApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
